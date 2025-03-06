6309c99523703160e60d2f3438627d6735603124
Paul Hlusko <paul.hlusko@gmail.com>
2025-03-06 08:02:11 -0500

    adding dreams

--- js/js.js
+++ js/js.js
@@ -1,21 +1,60 @@
-const SINGLE_VIDEO_DURATION = 290;
-const NUM_VIDEOS = 8;
-const TOTAL_DURATION = SINGLE_VIDEO_DURATION * NUM_VIDEOS;
-/*
-const apngList = [
-	"background_apngs/part1.apng",
-	"background_apngs/part2.apng",
-	"background_apngs/part3.apng",
-	"background_apngs/part4.apng",
-	"background_apngs/part5.apng",
-	"background_apngs/part6.apng",
-	"background_apngs/part7.apng",
-	"background_apngs/part8.apng"
-];
-*/
-const apngList = [
-	"background_apngs/dream1.apng"
-];
+const params = new URLSearchParams(window.location.search);
+const stage =  params.get("stage");
+var SINGLE_VIDEO_DURATION = 178;
+var NUM_VIDEOS = 13
+var apngList = [];
+switch (stage) {
+	case "filtered":
+		SINGLE_VIDEO_DURATION = 290;
+		NUM_VIDEOS = 8
+		TOTAL_DURATION = SINGLE_VIDEO_DURATION * NUM_VIDEOS;
+		apngList = [
+			"background_apngs/1.apng",
+			"background_apngs/2.apng",
+			"background_apngs/3.apng",
+			"background_apngs/4.apng",
+			"background_apngs/5.apng",
+			"background_apngs/6.apng",
+			"background_apngs/7.apng",
+			"background_apngs/8.apng"
+		];
+	break;
+	case "woke":
+		SINGLE_VIDEO_DURATION = 290;
+		NUM_VIDEOS = 8
+		TOTAL_DURATION = SINGLE_VIDEO_DURATION * NUM_VIDEOS;
+		apngList = [
+			"background_apngs/part1.apng",
+			"background_apngs/part2.apng",
+			"background_apngs/part3.apng",
+			"background_apngs/part4.apng",
+			"background_apngs/part5.apng",
+			"background_apngs/part6.apng",
+			"background_apngs/part7.apng",
+			"background_apngs/part8.apng"
+		];
+	break;
+	default:
+		SINGLE_VIDEO_DURATION = 178;
+		NUM_VIDEOS = 13
+		TOTAL_DURATION = SINGLE_VIDEO_DURATION * NUM_VIDEOS;
+		apngList = [
+			"background_apngs/segment_1.apng",
+			"background_apngs/segment_2.apng",
+			"background_apngs/segment_3.apng",
+			"background_apngs/segment_4.apng",
+			"background_apngs/segment_5.apng",
+			"background_apngs/segment_6.apng",
+			"background_apngs/segment_7.apng",
+			"background_apngs/segment_8.apng",
+			"background_apngs/segment_9.apng",
+			"background_apngs/segment_10.apng",
+			"background_apngs/segment_11.apng",
+			"background_apngs/segment_12.apng",
+			"background_apngs/segment_13.apng"
+		];
+	break;
+}
 document.addEventListener("DOMContentLoaded", () => {
 	var user = "silent8ch";
 	var domain = "gmail.com";
@@ -23,11 +62,34 @@ document.addEventListener("DOMContentLoaded", () => {
 	var link = document.getElementById("mail-link");
 	link.href = "mailto:" + email;
 	link.textContent = email;
+
+	var stageLink = document.getElementById("stage-link");
+	switch (stage) {
+		case "dream":
+			stageLink.href = "?stage=woke"
+			stageLink.textContent = "Wake up.";
+		break;
+		case "woke":
+			stageLink.href = "?stage=filtered"
+			stageLink.textContent = "Filter reality.";
+		break;
+		case "filtered":
+			stageLink.href = "?stage=dream"
+			stageLink.textContent = "Enter the dream.";
+		break;
+		default:
+			stageLink.href = "?stage=woke"
+			stageLink.textContent = "Wake up.";
+		break;
+	}
+	if (stage === "dream") {
+
+	} 
 	setBackgroundApng();
 	setTimeout(() => {
 	window.location.reload();
-	}, 290000);
-	const params = new URLSearchParams(window.location.search);
+	}, SINGLE_VIDEO_DURATION * 1000);
+	
     const playerVal = params.get("player");
 
     if (playerVal === "1") {
